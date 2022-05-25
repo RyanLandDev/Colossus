2af6fe5ab546c4baa6e2e9ed520ddb8de2d6b54f
RyanLand <ryanlandofficial@hotmail.com>
2022-05-25 21:38:14 +0200

    finish MongoDatabaseDriver implementation (untested)

--- src/main/java/net/ryanland/colossus/sys/file/database/MongoDatabaseDriver.java
+++ src/main/java/net/ryanland/colossus/sys/file/database/MongoDatabaseDriver.java
@@ -1,13 +1,39 @@
 package net.ryanland.colossus.sys.file.database;
 
-import net.dv8tion.jda.api.entities.ISnowflake;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import net.dv8tion.jda.api.entities.*;
+import net.ryanland.colossus.sys.file.serializer.MongoTableSerializer;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MongoDatabaseDriver extends DatabaseDriver {
-    //TODO
-    public MongoDatabaseDriver() {
 
+    private final String SECRET;
+    private final MongoClient CLIENT;
+    private final MongoDatabase DATABASE;
+
+    private final MongoCollection<Document> MEMBER_COLLECTION;
+    private final MongoCollection<Document> USER_COLLECTION;
+    private final MongoCollection<Document> GUILD_COLLECTION;
+    private final MongoCollection<Document> GLOBAL_COLLECTION;
+
+    public MongoDatabaseDriver(String secret, String databaseName) {
+        SECRET = secret;
+        CLIENT = MongoClients.create(SECRET);
+        DATABASE = CLIENT.getDatabase(databaseName);
+
+        MEMBER_COLLECTION = DATABASE.getCollection("members");
+        USER_COLLECTION = DATABASE.getCollection("users");
+        GUILD_COLLECTION = DATABASE.getCollection("guilds");
+        GLOBAL_COLLECTION = DATABASE.getCollection("global");
     }
 
     /**
@@ -18,7 +44,22 @@ public class MongoDatabaseDriver extends DatabaseDriver {
      */
     @Override
     protected List<TableCache<? extends ISnowflake>> getCaches() {
-        return null;
+        return List.of(
+            new TableCache<>(Member.class), new TableCache<>(User.class),
+            new TableCache<>(Guild.class), new TableCache<>(SelfUser.class) // SelfUser = global
+        );
+    }
+
+    private <T extends ISnowflake> MongoCollection<Document> getCollection(T client) {
+        if (client instanceof Member)
+            return MEMBER_COLLECTION;
+        if (client instanceof SelfUser)
+            return GLOBAL_COLLECTION;
+        if (client instanceof User)
+            return USER_COLLECTION;
+        if (client instanceof Guild)
+            return GUILD_COLLECTION;
+        throw new IllegalArgumentException();
     }
 
     /**
@@ -29,8 +70,11 @@ public class MongoDatabaseDriver extends DatabaseDriver {
      * @return The found table, {@code null} if it doesn't exist
      */
     @Override
+    @SuppressWarnings("all")
     protected <T extends ISnowflake> Table<T> findTable(T client) {
-        return null;
+        Document document = getCollection(client).find(Filters.eq("_id", client.getId())).first();
+        if (document == null) return null;
+        return (Table<T>) MongoTableSerializer.getInstance().deserialize(document);
     }
 
     /**
@@ -42,7 +86,9 @@ public class MongoDatabaseDriver extends DatabaseDriver {
      */
     @Override
     protected <T extends ISnowflake> Table<T> insertTable(T client, Table<T> table) {
-        return null;
+        Document document = MongoTableSerializer.getInstance().serialize(table);
+        getCollection(client).insertOne(document);
+        return table;
     }
 
     /**
@@ -52,7 +98,7 @@ public class MongoDatabaseDriver extends DatabaseDriver {
      */
     @Override
     protected <T extends ISnowflake> void deleteTable(T client) {
-
+        getCollection(client).deleteOne(Filters.eq("_id", client.getId()));
     }
 
     /**
@@ -63,6 +109,17 @@ public class MongoDatabaseDriver extends DatabaseDriver {
      */
     @Override
     public <T extends ISnowflake> void updateTable(T client, Table<T> table) {
+        getCollection(client).updateOne(Filters.eq("e", 1), getUpdates(findTable(client), table));
+    }
 
+    private <T extends ISnowflake> List<Bson> getUpdates(Table<T> oldTable, Table<T> newTable) {
+        List<Bson> updates = new ArrayList<>();
+        // repeat over every value in the new table, if it's not equal to the value in the old table, add an update
+        newTable.getRawData().forEach((key, value) -> {
+            if (oldTable.getRawData().get(key) != value) {
+                updates.add(Updates.set(key, value));
+            }
+        });
+        return updates;
     }
 }
