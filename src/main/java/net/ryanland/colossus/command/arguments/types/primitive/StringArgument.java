2dc0473ca056b20f678243fa8888f064c06f39c1
RyanLand <ryanlandofficial@hotmail.com>
2022-09-23 21:58:10 +0200

    fix stringargument

--- src/main/java/net/ryanland/colossus/command/arguments/types/primitive/StringArgument.java
+++ src/main/java/net/ryanland/colossus/command/arguments/types/primitive/StringArgument.java
@@ -16,8 +16,8 @@ public class StringArgument extends ArgumentStringResolver<String> {
         return data;
     }
 
-    private Integer min;
-    private Integer max;
+    private Integer min = 0;
+    private Integer max = 4000;
 
     public StringArgument() {
     }
