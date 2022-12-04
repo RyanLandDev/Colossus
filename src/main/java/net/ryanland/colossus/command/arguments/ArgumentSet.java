4463fff07197d3a2d7b5933d7ab5c3689da5e046
RyanLand <ryanlandofficial@hotmail.com>
2022-12-04 21:30:57 +0100

    fix issue where arguments weren't in the correct order

--- src/main/java/net/ryanland/colossus/command/arguments/ArgumentSet.java
+++ src/main/java/net/ryanland/colossus/command/arguments/ArgumentSet.java
@@ -1,14 +1,13 @@
 package net.ryanland.colossus.command.arguments;
 
-import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class ArgumentSet extends LinkedHashMap<String, Argument<?>> {
 
     public ArgumentSet addArguments(Argument<?>... arguments) {
-        putAll(Arrays.stream(arguments).collect(Collectors.toMap(Argument::getName, Function.identity())));
+        for (Argument<?> argument : arguments) {
+            put(argument.getName(), argument);//don't use streams to retain order
+        }
         return this;
     }
 }
