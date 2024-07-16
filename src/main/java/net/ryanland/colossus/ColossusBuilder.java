09a73b969b4b16c4cb66c4839119377a17a80952
RyanLand <ryanlandofficial@hotmail.com>
2024-07-17 00:57:16 +0200

    auto config generator should now include custom entries

--- src/main/java/net/ryanland/colossus/ColossusBuilder.java
+++ src/main/java/net/ryanland/colossus/ColossusBuilder.java
@@ -236,7 +236,10 @@ public class ColossusBuilder {
                 new SQLGuildsProvider(), new SQLMembersProvider(), new SQLUsersProvider(), new SQLUsersProvider.CooldownsProvider());
         }
 
-        if (configDirectory != null) config.read();
+        if (configDirectory != null) {
+            config.addValues(configEntries);
+            config.read();
+        }
 
         return new Colossus(jdaBuilder, config, categories, commands, contextCommands, localFiles,
             buttonListenerExpirationTimeAmount, buttonListenerExpirationTimeUnit, databaseDriver, providers,
